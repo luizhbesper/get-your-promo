ed3551a99c2bb8834e3ee20b5a6242786011cd02
Luiz Esper <luiz.hb.esper@gmail.com>
2025-03-05 23:28:24 -0300

    fix: upload layout updated

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -82,19 +82,17 @@ const Upload = () => {
 
     return (
         <div className="flex flex-col h-screen">
-            <div className="p-12">
-                <h1 className="text-2xl md:text-3xl font-bold text-left md:w-xl lg:w-3xl">
-                    Upload a Promo
-                </h1>
-            </div>
+            <div className="flex-1 flex items-center justify-center p-12">
+                <div className="w-full max-w-4xl flex flex-col">
+                    <h1 className="text-2xl md:text-3xl font-bold mb-8">
+                        Upload a Promo
+                    </h1>
 
-            <div className="flex-1 flex items-center justify-center">
-                <div className="w-full flex items-center justify-center"> 
                     {!showForm ? (
-                        <div className="flex flex-col w-2xl items-center justify-center">
+                        <div className="flex flex-col items-center justify-center">
                             <div
                                 {...getRootProps()}
-                                className={`w-2xl h-100 flex items-center justify-center rounded-lg rounded-b-none p-12 cursor-pointer border border-zinc-800 transition-colors ${
+                                className={`w-full h-100 flex items-center justify-center rounded-lg rounded-b-none p-12 cursor-pointer border border-zinc-800 transition-colors ${
                                     isDragActive 
                                         ? 'bg-zinc-600 border-zinc-600' 
                                         : 'bg-zinc-800 hover:border-zinc-600'
@@ -102,7 +100,7 @@ const Upload = () => {
                             >
                                 <input {...getInputProps()} />
                                 <div className="flex flex-col items-center justify-center gap-4 font-bold">
-                                    <UploadSimple size={48} className="text-gray-400" />
+                                    <UploadSimple size={48} className="text-zinc-400" />
                                     <p className="text-zinc-400">
                                         Drag and drop your track file here
                                     </p>
@@ -112,7 +110,7 @@ const Upload = () => {
                                 </div>
                             </div>
 
-                            <div className="w-2xl h-2 bg-zinc-200 rounded-b-lg">
+                            <div className="w-full h-2 bg-zinc-200 rounded-b-lg">
                                 <div
                                     className="h-full bg-cyan-500 transition-all duration-200"
                                     style={{ width: `${uploadProgress}%` }}
@@ -120,99 +118,108 @@ const Upload = () => {
                             </div>
                         </div>
                     ) : (
-                        <form onSubmit={handleSubmit(onSubmit)} className="rounded-xl p-8">
-                            <div className="flex gap-8">
-                                <div
-                                    {...getArtworkRootProps()}
-                                    className={`w-64 h-64 flex items-center justify-center rounded-lg cursor-pointer border border-zinc-800 transition-colors ${
-                                        isArtworkDragActive 
-                                            ? 'bg-zinc-600 border-zinc-600' 
-                                            : 'bg-zinc-800 hover:border-zinc-600'
-                                    }`}
-                                >
-                                    <input {...getArtworkInputProps()} />
-                                    {artworkFile ? (
-                                        <img
-                                            src={URL.createObjectURL(artworkFile)}
-                                            alt="Artwork"
-                                            className="w-full h-full object-cover rounded-lg"
-                                        />
-                                    ) : (
-                                        <div className="flex flex-col items-center justify-center gap-2">
-                                            <Image size={100} className="text-zinc-400" weight="thin"/>
-                                            <p className="text-zinc-400 text-center">Drag and Drop track's artwork</p>
+                        <div className="bg-zinc-900 rounded-xl p-8">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="flex gap-8">
+                                    <div className="flex flex-col gap-4">
+                                        <div
+                                            {...getArtworkRootProps()}
+                                            className={`w-64 h-64 flex items-center justify-center rounded-lg cursor-pointer border border-zinc-800 transition-colors ${
+                                                isArtworkDragActive 
+                                                    ? 'bg-zinc-600 border-zinc-600' 
+                                                    : 'bg-zinc-800 hover:border-zinc-600'
+                                            }`}
+                                        >
+                                            <input {...getArtworkInputProps()} />
+                                            {artworkFile ? (
+                                                <img
+                                                    src={URL.createObjectURL(artworkFile)}
+                                                    alt="Artwork"
+                                                    className="w-full h-full object-cover rounded-lg"
+                                                />
+                                            ) : (
+                                                <div className="flex flex-col items-center justify-center gap-2 p-4">
+                                                    <Image size={100} className="text-zinc-400" weight="thin"/>
+                                                    <p className="text-zinc-400 text-center text-sm">
+                                                        Drag and Drop track's artwork
+                                                    </p>
+                                                </div>
+                                            )}
                                         </div>
-                                    )}
-                                </div>
-
-                                <div className="flex-1 flex flex-col gap-4 w-120">
-                                    <div>
-                                        <span className="text-zinc-200">Title</span>
-                                        <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
-                                            <input
-                                                {...register("title", { required: true })}
-                                                placeholder="Title of the track"
-                                                className="w-full bg-transparent text-zinc-100 outline-none"
-                                            />
-                                        </div>
+                                        <span className="text-zinc-400 text-sm text-center">
+                                            Recommended size: 1000x1000px
+                                        </span>
                                     </div>
 
-                                    <div>
-                                        <span className="text-zinc-200">Artists</span>
-                                        <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
-                                            <input
-                                                {...register("artists", { required: true })}
-                                                placeholder="Artists"
-                                                className="w-full bg-transparent text-zinc-100 outline-none"
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <span className="text-zinc-200">Tags</span>
-                                        <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
-                                            <input
-                                                {...register("tags", { required: true })}
-                                                placeholder="Tag track's genre or any tag you want"
-                                                className="w-full bg-transparent text-zinc-100 outline-none"
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <span className="text-zinc-200">Track file</span>
-                                        <div className="flex items-center justify-between p-3 bg-zinc-700 rounded-lg mt-2">
-                                            <div className="flex items-center gap-3">
-                                                <span className="text-sm text-zinc-400">{uploadedFile?.name}</span>
+                                    <div className="flex-1 flex flex-col gap-4">
+                                        <div>
+                                            <span className="text-zinc-200">Title</span>
+                                            <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
+                                                <input
+                                                    {...register("title", { required: true })}
+                                                    placeholder="Title of the track"
+                                                    className="w-full bg-transparent text-zinc-100 outline-none"
+                                                />
                                             </div>
+                                        </div>
+
+                                        <div>
+                                            <span className="text-zinc-200">Artists</span>
+                                            <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
+                                                <input
+                                                    {...register("artists", { required: true })}
+                                                    placeholder="Artists"
+                                                    className="w-full bg-transparent text-zinc-100 outline-none"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <span className="text-zinc-200">Tags</span>
+                                            <div className="flex items-center p-3 bg-zinc-700 rounded-lg mt-2">
+                                                <input
+                                                    {...register("tags", { required: true })}
+                                                    placeholder="Tag track's genre or any tag you want"
+                                                    className="w-full bg-transparent text-zinc-100 outline-none"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <span className="text-zinc-200">Track file</span>
+                                            <div className="flex items-center justify-between p-3 bg-zinc-700 rounded-lg mt-2">
+                                                <div className="flex items-center gap-3">
+                                                    <span className="text-sm text-zinc-400">{uploadedFile?.name}</span>
+                                                </div>
+                                                <button
+                                                    type="button"
+                                                    onClick={removeFile}
+                                                    className="text-zinc-400 hover:text-red-500 transition-colors cursor-pointer"
+                                                >
+                                                    <X size={20} />
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                        <div className="flex justify-end gap-4 mt-4">
                                             <button
                                                 type="button"
-                                                onClick={removeFile}
-                                                className="text-zinc-400 hover:text-red-500 transition-colors cursor-pointer"
+                                                onClick={() => setCurrentScreen('promos')}
+                                                className="px-3 py-2 border border-zinc-500 text-zinc-500 rounded-lg hover:bg-red-500 hover:text-zinc-100 hover:border-red-500 transition-colors cursor-pointer"
                                             >
-                                                <X size={20} />
+                                                Discard
+                                            </button>
+                                            <button
+                                                type="submit"
+                                                className="px-3 py-2 bg-cyan-500 text-white rounded-lg hover:bg-cyan-600 transition-colors cursor-pointer"
+                                            >
+                                                Save
                                             </button>
                                         </div>
                                     </div>
-
-                                    <div className="flex justify-end gap-4 mt-4">
-                                        <button
-                                            type="button"
-                                            onClick={() => setCurrentScreen('promos')}
-                                            className="px-3 py-2 border border-zinc-500 text-zinc-500 rounded-lg hover:bg-red-500 hover:text-zinc-100 hover:border-red-500 transition-colors cursor-pointer"
-                                        >
-                                            Discard
-                                        </button>
-                                        <button
-                                            type="submit"
-                                            className="px-3 py-2 bg-cyan-500 text-white rounded-lg hover:bg-cyan-600 transition-colors cursor-pointer"
-                                        >
-                                            Save
-                                        </button>
-                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     )}
                 </div>
             </div>
